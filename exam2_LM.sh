c0ac22e13ffaa5840f5820c892e258a9955e113d
Lauren Michael <lemichael1213@gmail.com>
2023-04-05 11:17:28 -0400

    exam2_LM.sh

--- exam2_LM.sh
+++ exam2_LM.sh
@@ -1,51 +1,67 @@
+#Lauren Michael
+#Big Data Analysis Exam 2
+#4/5/23
 
 
+#Question 1
 
-#we need to cut down the file to just what we need: location, year, and species code
-#1 is location ID, 10 is year, and 12 is species code
+#To begin, I made a directory for question 1 csvs to be stored in
+# then we need to cut down the file to just what we need: year and species code
+#10 is year, 12 is species code
 #we then want to sort by the second column (year) and third column (species) in the cut file
 #-n orders from earliest year to latest
-#next, uniq -u should pull out unique values and we use -f 1 to skip the first column
+#next, uniq -u should pull out unique value
 
-#QUESTION 1
-mkdir exam2_question1
+
+#mkdir exam2_question1_bigdata
 for item in ../shared/birds/*.csv
 do
-cat $item | cut -f 10,12 -d ',' | sort -k2 -k3 -n | uniq -u >> ~/exam2_question1/exam2_question1_unique.csv
+cut -f 10,12 -d ',' | sort -k1 -k2 -n | uniq -u >> ~/exam2_question1_bigdata/exam2_question1_unique.csv
 done
  
 #this loop should then get us all of the associate info for those unique observations (including location id)
-while read line
-do
-  grep $line ../shared/birds/*.csv >> ~/exam2_question1/exam2_question1_output.csv  
-done <exam2_question1_unique.csv 
 
-#now need two more loops...one for genus and one for location. figure out how to grep
-
-for species 
+while read -r line
 do
-grep 
+  grep -Fwf $line ../shared/birds/*.csv >> ~/exam2_question1_bigdata/exam2_question1_output.csv  
+done < ~/exam2_question1_bigdata/exam2_question1_unique.csv 
 
-for species in ../~/exam2_question1/exam2_question1combined.csv
-do
-grep 
+grep -Fwf <(cut -d ',' -f 1 ~/exam2_question1_bigdata/exam2_question1_output.csv) ../shared/birds/bird-info # >> birdinfo.csv
+#now we use grep to gather the genus and location info
+
+
+#this should bring in the bird info 
+awk '{print $12}' ~/exam2_question1_bigdata/exam2_question1_output.csv | grep ../shared/birds/bird-info >> ~/exam2_question1_bigdata/exam2_question1_bird_info
+
+#this should bring in the site info
+awk '{print $12}' ~/exam2_question1_bigdata/exam2_question1_output.csv | grep ../shared/birds/observation-site-info >> ~/exam2_question1_bigdata/exam2_question1_site_info
+
 
 #QUESTION 2
-mkdir exam2_question2
+
+#To begin, I made a directory for question 2 csvs to be stored in
+# then we need to cut down the file to just what we need: year and species code
+#10 is year, 12 is species code
+#we then want to sort by the second column (year) and third column (species) in the cut file
+#-n orders from earliest year to latest
+#next, uniq -u should pull out unique value
+
+#mkdir exam2_question2_bigdata
 for item in ../shared/birds/*.csv
 do
-cat $item > exam2question2.csv | cut -f 1,10,12 -d ',' | sort -k3 -n | uniq -u -f 1 > ~/exam2_question2/exam2_question2.csv
+cat $item | cut -f 10,12 -d ',' | sort -k2 -k3 -n | uniq -u >> ~/exam2_question2_bigdata/exam2_question2_unique.csv
 done
 
+#this loop should then get us all of the associate info for those unique observations (including location id)
 
+while read line
+do
+  grep $line ../shared/birds/*.csv >> ~/exam2_question2_bigdata/exam2_question2_output.csv
+done < ~/exam2_question2_bigdata/exam2_question2_unique.csv
 
+#this should bring in the bird info
+awk '{print $12}' ~/exam2_question2_bigdata/exam2_question2_output.csv | grep ../shared/birds/bird-info >> ~/exam2_question2_bigdata/exam2_question2_bird_info
 
-#how do I tell it to just do unique of species?
-#I tried using -f 2 to tell it to skip the first two fields, is this right?
-
-#for part 2, would I run the same loop but remove sort?
-
-#What command allows me to bring in the values from the bird info and observation csvs
-#Can all of this be done in two loops (one per question?)
-#Does she want us to do the {1} thing that makes the script generic?
+#this should bring in the site info
+awk '{print $12}' ~/exam2_question2_bigdata/exam2_question2_output.csv | grep ../shared/birds/observation-site-info >> ~/exam2_question1_bigdata/exam2_question2$
 
